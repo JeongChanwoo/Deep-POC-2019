5540fb4a2c3ce1b0482a296f1049eb817cad6752
ChanwooJeong <chanwooda0223@gmail.com>
2020-01-06 16:45:11 +0900

    shell script 기반의 one shot 도커 pull, 셋팅, 학습, 포스트 프로세싱, bento 생성을 하나의 shell script 실행으로 수정

--- train_serve.sh
+++ train_serve.sh
@@ -8,5 +8,10 @@ docker pull chanwooda0223/marker1010-semantic-segmentation-serving
 
 
 volume=$DIR
+echo $volume
 
-docker run --gpus all -p 8889:8888 -p 8891:5000 -v volume/:/notebook -td train-serving
+# docker run --gpus all -p 8893:8888 -p 8894:5000 -v volume/:/notebook -td train-serving /bin/bash
+
+docker run --rm --gpus all -v $volume/:/notebook -t marker1010-semantic-segmentation-serving /bin/sh trainer.sh
+
+
